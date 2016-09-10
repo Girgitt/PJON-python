f978bb3dcb1bcabcf8b81406c1d4e43da21c8e4f
Zbigniew Zasieczny <zasiecznyz@tt.com.pl>
2016-09-10 17:15:49 +0200

    added setup files to register module on PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='PJON-python',
+  packages=['PJON-python'],
+  version='4.2.0',
+  description='Python implementation of the PJON communication protocol.',
+  author='Zbigniew Zasieczny',
+  author_email='z.zasieczny@gmail.com',
+  url='https://github.com/Girgitt/PJON-python',
+  download_url='https://github.com/Girgitt/PJON-python/tarball/4.2.0',
+  keywords=['PJON', 'multimaster', 'serial', 'RS485', 'arduino'],
+  classifiers=[],
+)
