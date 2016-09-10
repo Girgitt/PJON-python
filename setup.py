7f492575f769049eb43137e7d67b37faae31b83a
Zbigniew Zasieczny <zasiecznyz@tt.com.pl>
2016-09-10 17:38:47 +0200

    changed package name in setup.py to match module directory's name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name='PJON-python',
-  packages=['PJON-python'],
-  version='4.2.0',
+  name='pjon_python',
+  packages=['pjon_python'],
+  version='4.2.1',
   description='Python implementation of the PJON communication protocol.',
   author='Zbigniew Zasieczny',
   author_email='z.zasieczny@gmail.com',
   url='https://github.com/Girgitt/PJON-python',
-  download_url='https://github.com/Girgitt/PJON-python/tarball/4.2.0',
+  download_url='https://github.com/Girgitt/PJON-python/tarball/4.2.1',
   keywords=['PJON', 'multimaster', 'serial', 'RS485', 'arduino'],
   classifiers=[],
 )
