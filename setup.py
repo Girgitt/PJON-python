212bfe0fece9b07db09ecf436367e0c4b2b6fc68
Zbigniew Zasieczny <zasiecznyz@tt.com.pl>
2016-09-10 17:52:08 +0200

    fixed sub module packages definition

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name='pjon_python',
-  packages=['pjon_python'],
-  version='4.2.1',
+  packages=['pjon_python', 'pjon_python.protocol', 'pjon_python.strategies', 'pjon_python.utils'],
+  version='4.2.2',
   description='Python implementation of the PJON communication protocol.',
   author='Zbigniew Zasieczny',
   author_email='z.zasieczny@gmail.com',
   url='https://github.com/Girgitt/PJON-python',
-  download_url='https://github.com/Girgitt/PJON-python/tarball/4.2.1',
+  download_url='https://github.com/Girgitt/PJON-python/tarball/4.2.2',
   keywords=['PJON', 'multimaster', 'serial', 'RS485', 'arduino'],
   classifiers=[],
 )
